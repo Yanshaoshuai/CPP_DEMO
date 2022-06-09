ff2777ff0340f95d7d6f6b29d1172810b150dcf0
yan <ahutyss@gmail.com>
2022-06-09 23:52:43 +0800

    add some csapp test case

--- csapp/data_expression.c
+++ csapp/data_expression.c
@@ -75,3 +75,127 @@ void test_ascii()
     const char *S = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     show_bytes(S, strlen(S));
 }
+
+void test_bit_calculate()
+{
+    // 0x41=[0100 1011]
+    unsigned char a = 0x4b;
+    // 0x14=[0001 0111]
+    unsigned char b = 0x17;
+    // a&b=[0000 0011]
+    printf("a&b=%.2x\n", a & b);
+    // a|b=[0101 1111]
+    printf("a|b=%.2x\n", a | b);
+    // a^b=[0101 1100]
+    printf("a^b=%.2x\n", a ^ b);
+
+    //~a=[1011 0100]
+    printf("~a=%.2x\n", ((unsigned char)(~a)));
+    //~b=[1110 1000]
+    printf("~b=%.2x\n", ((unsigned char)(~b)));
+}
+
+void inplace_swap(int *x, int *y)
+{
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+}
+
+void test_inplace_swap()
+{
+    int x = 1;
+    int y = 2;
+    inplace_swap(&x, &y);
+    printf("x=%d,y=%d\n", x, y);
+}
+
+/**
+ * 反转数组
+ */
+void reverse_array(int a[], int count)
+{
+    int first, last;
+    for (first = 0, last = count - 1; first < last; first++, last--)
+    {
+        inplace_swap(&a[first], &a[last]);
+    }
+}
+
+void test_reverse_array()
+{
+    int a[] = {1, 3, 2, 5, 4};
+    reverse_array(a, sizeof(a) / sizeof(int));
+    for (size_t i = 0; i < sizeof(a) / sizeof(int); i++)
+    {
+        printf(" %d ", a[i]);
+    }
+    printf("\n");
+}
+
+/**
+ * 位级运算和逻辑运算
+ */
+void diff_bit_logic()
+{
+    unsigned char x = 0x66;
+    unsigned char y = 0x39;
+    //逻辑运算的结果只有0或1
+    printf("x&y=%.2x\n", ((unsigned char)(x & y)));
+    printf("x|y=%.2x\n", ((unsigned char)(x | y)));
+    printf("~x|~y=%.2x\n", ((unsigned char)(~x | ~y)));
+    printf("x&!y=%.2x\n", ((unsigned char)(x & !y)));
+    printf("x&&y=%.2x\n", ((unsigned char)(x && y)));
+    printf("x||y=%.2x\n", ((unsigned char)(x || y)));
+    printf("!x||!y=%.2x\n", ((unsigned char)(!x || !y)));
+    printf("x&&~y=%.2x\n", ((unsigned char)(x && ~y)));
+
+    //逻辑运算 &&从左到右的表达式有一个为0就停止计算结果为0
+    //|| 从左到右的表达式有一个为1就停止计算结果为1
+    int count = 0;
+    0 && ++count;
+    printf("count=%d\n", count);
+    1 && ++count;
+    printf("count=%d\n", count);
+    0 || ++count;
+    printf("count=%d\n", count);
+    1 || ++count;
+    printf("count=%d\n", count);
+}
+
+/**
+ * 实现x==y
+ */
+int eq(int x, int y)
+{
+    return !(x ^ y);
+}
+void test_eq()
+{
+    printf("1==2 =%d\n", eq(1, 2));
+    printf("2==2 =%d\n", eq(2, 2));
+}
+
+/**
+ * 移位运算
+ */
+void move_bit()
+{
+
+    // x=[0110 0011] y=[1001 0101]
+    unsigned char x = 0x63;
+    unsigned char y = 0x95;
+    //左移4位 [0011 1001] [0101 0000]
+    printf("x<<4=%.2x,y<<4=%.2x\n", ((unsigned char)(x << 4)), ((unsigned char)(y << 4)));
+    x = 0x63;
+    y = 0x95;
+    //右移4位[0000 0110] [0011 1001]
+    printf("x>>4=%.2x,y>>4=%.2x\n", ((unsigned char)(x >> 4)), ((unsigned char)(y >> 4)));
+
+    //有符号数只有算术右移(c只有算术右移=>每字节高位用符号位补齐)
+    int a = 0x63;
+    int b = 0x95;
+    //逻辑右移4位[0000 0110] [1111 1001]
+    // printf("a=%.2x,b=%.2x\n",((unsigned int)a), ((unsigned int)b));
+    printf("a>>4=%x,b>>4=%x\n", a >> 4, b >> 4);
+}
--- csapp/main.c
+++ csapp/main.c
@@ -5,6 +5,12 @@ int main(int argc, char const *argv[])
     // byte_order();
     // test_show_bytes(12345);
 
-    test_ascii();
+    // test_ascii();
+    // test_bit_calculate();
+    // test_inplace_swap();
+    // test_reverse_array();
+    // diff_bit_logic();
+    // test_eq();
+    move_bit();
     return 0;
 }
